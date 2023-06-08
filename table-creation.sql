fb577271620dca269764cebea02369b33ab9f8c8
elias <rojas.elias@outlook.com>
2023-06-08 15:36:56 -0300

    design simplified, php for automating data entry added

--- table-creation.sql
+++ table-creation.sql
@@ -3,20 +3,13 @@ DROP TABLE IF EXISTS idioma_pelicula;
 DROP TABLE IF EXISTS peliculas;
 DROP TABLE IF EXISTS titulo_pelicula;
 DROP TABLE IF EXISTS titulos_de_peliculas;
---DROP TABLE IF EXISTS departamentos;
---DROP TABLE IF EXISTS referencias_peliculas;
---DROP TABLE IF EXISTS historial_ediciones;
---DROP TABLE IF EXISTS razon_edicion;
---DROP TABLE IF EXISTS tablas_modificables;
---DROP TABLE IF EXISTS personas;
---DROP TABLE IF EXISTS trabajan;
---DROP TABLE IF EXISTS usuarios;
---DROP TABLE IF EXISTS paises;
---DROP TABLE IF EXISTS roles;
---DROP TABLE IF EXISTS genero_pelicula;
---DROP TABLE IF EXISTS generos_de_peliculas;
---DROP TABLE IF EXISTS razones_de_edicion;
---DROP TABLE IF EXISTS puntuaciones;
+DROP TABLE IF EXISTS departamentos;
+DROP TABLE IF EXISTS referencias_peliculas;
+DROP TABLE IF EXISTS personas;
+DROP TABLE IF EXISTS trabajan;
+DROP TABLE IF EXISTS roles;
+DROP TABLE IF EXISTS genero_pelicula;
+DROP TABLE IF EXISTS generos_de_peliculas;
 
 
 CREATE TABLE peliculas(
@@ -53,94 +46,43 @@ CREATE TABLE titulos_de_peliculas(
   titulo VARCHAR(128)
 );
 
-CREATE TABLE resumen_pelicula(
-  id_pelicula INT,
-  id_idioma INT,
-  resumen VARCHAR(1024) NOT NULL, -- se rellenara con una string vacia si no tiene titulo
+
+CREATE TABLE trabajan(
+  id_pelicula INT NOT NULL,
+  id_rol INT NOT NULL,
+  id_departamento INT NOT NULL,
+  id_persona INT NOT NULL
+);
+
+CREATE TABLE personas(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  fecha_nacimiento DATE,
 );
 
 
---CREATE TABLE historial_ediciones(
---  id INT,
---  id_usuario INT NOT NULL,
---  id_pelicula INT,
---  id_persona INT,
---  tabla_modificada INT,
---  CONSTRAINT historial_ediciones_pk PRIMARY KEY(id,tabla_modificada)
---);
+CREATE TABLE roles(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(64) NOT NULL
+);
 
---CREATE TABLE tablas_modificables(
---  id INT AUTO_INCREMENT PRIMARY KEY,
---  nombre VARCHAR(32) NOT NULL
---);
---
---CREATE TABLE usuarios(
---  id INT,
---  fecha_nacimiento DATE,
---  fecha_creacion DATE,
---  nombre VARCHAR(128) NOT NULL
---);
---
---
---CREATE TABLE trabajan(
---  id_pelicula INT NOT NULL,
---  id_rol INT NOT NULL,
---  id_departamento INT NOT NULL,
---  id_persona INT NOT NULL
---);
---
---CREATE TABLE personas(
---  id INT AUTO_INCREMENT PRIMARY KEY,
---  fecha_nacimiento DATE,
---  pais_nacimiento INT NOT NULL
---);
---
---CREATE TABLE paises(
---  id INT AUTO_INCREMENT PRIMARY KEY,
---  nombre VARCHAR(64) NOT NULL
---);
---
---CREATE TABLE roles(
---  id INT AUTO_INCREMENT PRIMARY KEY,
---  nombre VARCHAR(64) NOT NULL
---);
---
---CREATE TABLE departamentos(
---  id INT AUTO_INCREMENT PRIMARY KEY,
---  nombre VARCHAR(64) NOT NULL
---);
---
---
---CREATE TABLE genero_pelicula(
---  id INT,
---  id_genero_de_pelicula INT,
---);
---
---CREATE TABLE generos_de_peliculas(
---  id INT AUTO_INCREMENT PRIMARY KEY,
---  nombre VARCHAR(32) NOT NULL
---);
---
---CREATE TABLE razon_edicion(
---  id_edicion INT,
---  id_razon_de_edicion INT,
---);
+CREATE TABLE departamentos(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(64) NOT NULL
+);
 
 
---CREATE TABLE referencias_peliculas(
---  id_referenciadora INT,
---  id_referenciada INT
---);
---
---CREATE TABLE razones_de_edicion(
---  id INT AUTO_INCREMENT PRIMARY KEY,
---  razon VARCHAR(64) NOT NULL
---);
---
---CREATE TABLE puntuaciones(
---  id_pelicula INT,
---  id_usuario INT,
---  puntuacion INT 
---);
---
---
+CREATE TABLE genero_pelicula(
+  id INT,
+  id_genero_de_pelicula INT,
+);
+
+CREATE TABLE generos_de_peliculas(
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nombre VARCHAR(32) NOT NULL
+);
+
+
+CREATE TABLE referencias_peliculas(
+  id_referenciadora INT,
+  id_referenciada INT
+);
