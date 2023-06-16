b49b76d245c99b1ff2f6304687eced21ce6186c2
elias <rojas.elias@outlook.com>
2023-06-16 18:33:16 -0300

    opa

--- consultas.sql
+++ consultas.sql
@@ -1,9 +1,18 @@
+-- 2)
+CREATE VIEW select_peliculas_con_un_actor_director AS
 SELECT id_pelicula,COUNT(*) "cuenta_actor_director" 
 FROM participaciones part 
 INNER JOIN participacion_rol partr ON partr.id_participacion = part.id
 INNER JOIN roles r ON r.id = partr.id_rol
 WHERE r.nombre IN ("actor principal","director")
 GROUP BY part.id HAVING cuenta_actor_director >=2 ;
+
+SELECT tdp.titulo 
+FROM peliculas_con_actor_director pad
+INNER JOIN titulo_pelicula tp
+ON tp.id_pelicula=pad.id_pelicula
+INNER JOIN titulos_de_peliculas tdp ON 
+tdp.id=tp.id_titulo GROUP BY tp.id_pelicula;
 
 -- El group by, en teoria forma grupos (que quedaran en especies de tablas nuevas) que tienen los mismos 
 -- valores en determinada columna seleccionada, luego "colapsa" todo en un registro y termino no teniendo
@@ -16,3 +25,26 @@ GROUP BY part.id HAVING cuenta_actor_director >=2 ;
 
 -- CREATE VIEW myview AS ...query... me deja guardar vistas, o sea consultas
 -- del tipo select
+
+
+-- 3)
+SELECT tdp.titulo, COUNT(*) "numero_actores_que_actuan"
+FROM participaciones part
+INNER JOIN participacion_rol partr ON part.id=partr.id_participacion 
+INNER JOIN roles r ON r.id=partr.id_rol
+INNER JOIN titulo_pelicula tp ON tp.id_pelicula = part.id_pelicula
+INNER JOIN titulos_de_peliculas tdp ON tdp.id=tp.id_titulo
+WHERE r.nombre IN ("actor principal","actor secundario") 
+GROUP BY part.id_pelicula;
+
+-- 6)
+SELECT AVG(duracion_minutos) 
+FROM peliculas p 
+WHERE p.fecha_estreno <"2000-01-01";
+
+-- 10
+SELECT gdp.nombre,COUNT(*) 
+FROM peliculas p 
+INNER JOIN genero_pelicula gp ON gp.id_pelicula=p.id
+INNER JOIN generos_de_peliculas gdp ON gdp.id = gp.id_genero_de_pelicula
+GROUP BY(gdp.nombre);
--- table-creation.sql
+++ table-creation.sql
@@ -10,6 +10,8 @@ DROP TABLE IF EXISTS participacion_rol;
 DROP TABLE IF EXISTS genero_pelicula;
 DROP TABLE IF EXISTS generos_de_peliculas;
 
+-- CREACION
+
 CREATE TABLE peliculas(
     id INT AUTO_INCREMENT PRIMARY KEY,
     fecha_estreno DATE NOT NULL,
@@ -81,3 +83,42 @@ CREATE TABLE participacion_rol(
     CONSTRAINT participacion_rol_pk PRIMARY KEY (id_participacion,id_rol)
 );
 
+-- ALTERS
+ALTER TABLE idioma_pelicula 
+ADD CONSTRAINT idioma_fk
+FOREIGN KEY (id_idioma) REFERENCES idiomas(id) ON DELETE CASCADE,
+ADD CONSTRAINT pelicula_fk 
+FOREIGN KEY (id_pelicula) REFERENCES peliculas(id) ON DELETE CASCADE;
+
+ALTER TABLE peliculas 
+ADD CONSTRAINT precuela_fk
+FOREIGN KEY (id_precuela) REFERENCES peliculas(id) ON DELETE CASCADE,
+ADD CONSTRAINT secuela_fk
+FOREIGN KEY (id_secuela) REFERENCES peliculas(id) ON DELETE CASCADE;
+
+ALTER TABLE genero_pelicula 
+ADD CONSTRAINT genero_pelicula_id_pelicula
+FOREIGN KEY (id_pelicula) REFERENCES peliculas(id) ON DELETE CASCADE,
+ADD CONSTRAINT genero_pelicula_id_genero_pelicula
+FOREIGN KEY (id_genero_de_pelicula) REFERENCES generos_de_peliculas(id) ON DELETE CASCADE;
+
+ALTER TABLE titulo_pelicula 
+ADD CONSTRAINT titulo_pelicula_p_fk
+FOREIGN KEY (id_pelicula) REFERENCES peliculas(id) ON DELETE CASCADE,
+ADD CONSTRAINT titulo_pelicula_t_fk
+FOREIGN KEY (id_titulo) REFERENCES titulos_de_peliculas(id) ON DELETE CASCADE,
+ADD CONSTRAINT titulo_pelicula_i_fk
+FOREIGN KEY (id_idioma) REFERENCES idiomas(id) ON DELETE CASCADE;
+
+
+ALTER TABLE participaciones 
+ADD CONSTRAINT participaciones_pelicula_fk
+FOREIGN KEY (id_pelicula) REFERENCES peliculas(id) ON DELETE CASCADE,
+ADD CONSTRAINT participaciones_persona_fk
+FOREIGN KEY (id_persona) REFERENCES personas(id) ON DELETE CASCADE;
+
+ALTER TABLE participacion_rol 
+ADD CONSTRAINT participacion_rol_id_rol
+FOREIGN KEY (id_rol) REFERENCES roles(id) ON DELETE CASCADE,
+ADD CONSTRAINT participacion_rol_id_participacion
+FOREIGN KEY (id_participacion) REFERENCES participaciones(id) ON DELETE CASCADE;
